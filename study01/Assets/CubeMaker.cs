9f0181e3811dd1caa60cb3895009793f05914a84
Lacty <akira206@gmail.com>
2016-05-12 17:50:09 +0900

    study01

---
+++ study01/Assets/CubeMaker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMaker : MonoBehaviour {
+  
+  // Use this for initialization
+  void Start () {
+    var obj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+  }
+
+	// Update is called once per frame
+  void Update () {
+  
+  }
+}
