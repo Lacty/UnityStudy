7e82eb6595d47f7bc763d2933e74f29a612b0c78
Lacty <akira206@gmail.com>
2016-05-16 09:13:10 +0900

    update study08: scene transition

---
+++ study08/Assets/startbutton.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class startbutton : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start ()
+  {
+    /* err
+     * the name "UnityEditor" dose not exist in the current context
+
+    UnityEditor
+      .Events
+      .UnityEventTools
+      .AddObjectPersistentListener<GameObject>
+      (GetComponent<Button>().onClick, OnClick, gameObject);
+    */
+
+    GetComponent<Button> ().onClick.AddListener (OnClick);
+  }
+	
+  // Update is called once per frame
+  void Update ()
+  {
+	
+  }
+
+  void OnClick() {
+    SceneManager.LoadSceneAsync ("game");
+    Debug.Log("click");
+  }
+}
