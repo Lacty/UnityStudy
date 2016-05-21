f987a0e0cad824e1b456bbd04983fb172219ba0d
Lacty <akira206@gmail.com>
2016-05-21 16:08:43 +0900

    update study14

--- study14/Assets/Player.cs
+++ study14/Assets/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
@@ -13,7 +14,7 @@ public class Player : MonoBehaviour {
   private float REDUCTION_RATE = 0.0f;
 
   [SerializeField]
-  private Vector3 velocity_ = new Vector3();
+  private Vector3 velocity_ = new Vector3 ();
 
   // Use this for initialization
   void Start ()
@@ -27,7 +28,6 @@ public class Player : MonoBehaviour {
   }
 
   // receive an Input to move the player
-  // 共通化できまくりだから後で修正かけろ
   void Move()
   {
     // up
@@ -35,35 +35,33 @@ public class Player : MonoBehaviour {
       velocity_.z += SPEED;
       velocity_.z = Mathf.Min (velocity_.z, MAX_SPEED);
     }
-    else {
-      velocity_.z *= REDUCTION_RATE;
-    }
+
     // down
     if (Input.GetKey (KeyCode.S)) {
       velocity_.z += -SPEED;
       velocity_.z = Mathf.Max (velocity_.z, -MAX_SPEED);
     }
-    else {
-      velocity_.z *= REDUCTION_RATE;
-    }
+
     // right
     if (Input.GetKey (KeyCode.D)) {
       velocity_.x += SPEED;
       velocity_.x = Mathf.Min (velocity_.x, MAX_SPEED);
     }
-    else {
-      velocity_.x *= REDUCTION_RATE;
-    }
+
     // left
     if (Input.GetKey (KeyCode.A)) {
       velocity_.x += -SPEED;
       velocity_.x = Mathf.Max (velocity_.x, -MAX_SPEED);
     }
-    else {
-      velocity_.x *= REDUCTION_RATE;
-    }
 
-    // move the player
-    transform.position += velocity_;
+    // set velocity
+    gameObject.GetComponent<Rigidbody>().velocity = velocity_;
+
+    Vector3 vel = gameObject.GetComponent<Rigidbody> ().velocity;
+
+    velocity_.x = vel.x * REDUCTION_RATE;
+    velocity_.z = vel.z * REDUCTION_RATE;
+
+    gameObject.GetComponent<Rigidbody>().velocity = velocity_;
   }
 }
