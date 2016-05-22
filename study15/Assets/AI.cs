9bc770b6bb8358a5c7c9c9260501836911f97cb0
Lacty <akira206@gmail.com>
2016-05-22 21:46:53 +0900

    update study15

---
+++ study15/Assets/AI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+  [SerializeField]
+  private float speed_ = 0.0f;
+
+  [SerializeField]
+  private Vector3 velocity_ = new Vector3();
+
+  [SerializeField]
+  private GameObject flags_ = null;
+  private GameObject Flags {
+    get { 
+      if (flags_ == null) {
+        flags_ = GameObject.Find ("Flags");
+      }
+      return flags_;
+    }
+  }
+
+  private GameObject flag_ = null;
+
+  [SerializeField]
+  private uint count_ = 0;
+
+	// Use this for initialization
+	void Start () {
+    flag_ = FindFlag (count_);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    velocity_ = (flag_.transform.position - transform.position).normalized * speed_;
+    gameObject.GetComponent<Rigidbody> ().velocity = velocity_;
+	}
+
+  void OnCollisionEnter(Collision collision) {
+    count_++;
+    if (Flags.GetComponent<Flags> ().Flag_Max == count_) {
+      count_ = 0;
+    }
+    flag_ = FindFlag (count_);
+  }
+
+  // 現在のFlagを取得
+  GameObject FindFlag(uint number) {
+    if (Flags.GetComponent<Flags> ().Flag_Max == number) {
+      return GameObject.Find ("Flags/Flag" + 0);
+    }
+    return GameObject.Find ("Flags/Flag" + number);
+  }
+}
---
+++ study15/Assets/Flags.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flags : MonoBehaviour {
+
+  [SerializeField]
+  private uint FLAG_MAX = 0;
+  public uint Flag_Max {
+    get{ return FLAG_MAX; }
+  }
+
+  [SerializeField]
+  private float RADIUS = 0.0f;
+
+  // Use this for initialization
+  void Start ()
+  {
+    for (int i = 0; i < FLAG_MAX; i++) {
+      var obj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+      obj.name = "Flag" + i;
+      obj.transform.SetParent (this.transform);
+      obj.transform.position = new Vector3 (Mathf.Sin((Mathf.PI * 2 / FLAG_MAX) * i) * RADIUS,
+                                            0,
+                                            Mathf.Cos((Mathf.PI * 2 / FLAG_MAX) * i) * RADIUS);
+    }
+  }
+	
+  // Update is called once per frame
+  void Update ()
+  {
+  }
+}
